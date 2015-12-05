db1ba69ce3e9763aa2e694e939ce5a18151b4234
Kelly Liu <kellyliu@dhcp-18-189-68-83.dyn.MIT.EDU>
2015-12-05 13:36:25 -0500

    added set methods to note class

--- src/models/Note.py
+++ src/models/Note.py
@@ -6,6 +6,18 @@ class Note():
 		self.modifiers = None;
 		pass
 
+
+	def set_pitch(self, type):
+		pass
+
+
+	def set_type(self, type):
+		pass
+
+
+	def set_modifiers(self, type):
+		pass
+
 class Pitch:
 	A,B,C,D,E,F,G = range(7)
 
