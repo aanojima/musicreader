b6171c97f098e362c63b9de3ef5b7b9971b916bf
Kelly Liu <kellyliu@dhcp-18-189-68-83.dyn.mit.edu>
2015-12-06 21:23:26 -0500

    writing code for centering all testing data...but idk how to do it yet

--- src/modules/classifyhelper.py
+++ src/modules/classifyhelper.py
@@ -3,6 +3,8 @@ import numpy as np
 from numpy.linalg import norm
 from PIL import Image
 
+from object_segment3 import *
+
 
 def preprocess_hog(x):
     samples = []
@@ -37,6 +39,19 @@ def preprocess_hog(x):
     # print "len samples", len(samples)
     return np.float32(samples)
 
+def center_data(x):
+	samples = []
+	print "x.shape", x.shape
+	numrows = x.shape[0]
+	numcols = x.shape[1]
+	for row in range(0,numrows):
+		for col in range(0,numcols):
+			img = x[row][col]
+			print findObjects(img)
+			samples.append(hist)
+	return np.float32(samples)
+
+
 # classify all given notes using the given knn
 # for testing purposes!!!! (includes pregiven testing labels)
 def classifyDebug(testingData, testingLabels, knn):
@@ -44,7 +59,8 @@ def classifyDebug(testingData, testingLabels, knn):
 	# test = testingData[:,:].reshape(-1, 400).astype(np.float32)
 	# print test.dtype
 	print "CLASSIFY DEBUG"
-	testing_data = preprocess_hog(testingData)
+	# testing_data = center_data(testingData)
+	testing_data = preprocess_hog(testing_data)
 	ret,result,neighbours,dist = knn.findNearest(testing_data,k=3)
 	print "result:\n", result
 	print "expected: \n", testingLabels
@@ -78,6 +94,7 @@ def getTrainingData(imageFileName):
 	cv_1 = cv_1[:, :, ::-1].copy()
 	cv_1 =  cv2.cvtColor(cv_1, cv2.COLOR_BGR2GRAY)
 	t1 = cv2.adaptiveThreshold(cv_1,255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 3)	
+	# need to center all data....
 	# cv2.imshow('thresholded', t1)
 	# cv2.waitKey(0)
 	return t1
@@ -100,8 +117,6 @@ def makeTestingData(imageNameList, width, height):
 		# cv2.imshow('thresholded', t1)
 		# cv2.waitKey(0)
 		# print"hello"
-		
-		
 		
 		# print "cells shape",  cells.shape
 		# print cells
--- src/modules/note.py
+++ src/modules/note.py
@@ -56,7 +56,6 @@ def trainData():
 	t1 = classifyhelper.getTrainingData("../../data/note_train/notes1.png")
 	t2 = classifyhelper.getTrainingData("../../data/note_train/img2.png")
 	t3 = classifyhelper.getTrainingData("../../data/note_train/img3.jpg")
-	
 	#SHARP, FLAT, NATURAL DATA THROW IT IN
 	t4 = classifyhelper.getTrainingData("../../data/accidental_train/acc1.jpg")
 	t5 = classifyhelper.getTrainingData("../../data/accidental_train/acc2.jpg")
@@ -65,25 +64,20 @@ def trainData():
 	cells1 = [np.hsplit(row, 4) for row in np.vsplit(t1, 9)]
 	cells2 = [np.hsplit(row, 4) for row in np.vsplit(t2, 9)]
 	cells3 = [np.hsplit(row, 4) for row in np.vsplit(t3, 9)]
-
 	# FOR SHARPS AND FLATS
 	cells4 = [np.hsplit(row, 6) for row in np.vsplit(t4, 9)]
 	cells5 = [np.hsplit(row, 7) for row in np.vsplit(t5, 9)]
 
 	cells = np.vstack((cells1, cells2, cells3))
 	x = np.array(cells);
-
 	# FOR SHARPS AND FLATS
-	# cells_accidental = np.vstack(cells4);
 	x2 = np.array(cells4)
-
-	# cells_accidental2 = np.vstack(cells5);
 	x3 = np.array(cells5)
 
-	# train = x[:, :].reshape(-1, 400).astype(np.float32)
 	train = classifyhelper.preprocess_hog(x)
 	train2 = classifyhelper.preprocess_hog(x2)
 	train3 = classifyhelper.preprocess_hog(x3)
+
 
 	# Make labels for train data
 	labels = np.array([WHOLE_RESULT, HALF_RESULT, QUARTER_RESULT, EIGHTH_RESULT])
@@ -95,15 +89,13 @@ def trainData():
 	labels_acc2 = np.array([NATURAL_RESULT, NATURAL_RESULT, NATURAL_RESULT, QUARTER_RESULT, QUARTER_RESULT, HALF_RESULT, HALF_RESULT])
 	train_labels_acc2 = np.tile(labels_acc2, 9)[:, np.newaxis]
 
-	print train_labels.shape
-	print train_labels_acc.shape
-	print train_labels_acc2.shape
-
-	train_length = train.shape[0] + train2.shape[0] + train3.shape[0]
+	
 	# combine training data into one array
 	# combine training labels into one array
+	train_length = train.shape[0] + train2.shape[0] + train3.shape[0]
 	train_final = np.empty((train_length, train.shape[1]), np.float32)
 	train_labels_final = np.empty((train_length, 1), np.float32)
+	
 
 	len1 = train.shape[0]
 	len2 = train2.shape[0]
@@ -114,76 +106,12 @@ def trainData():
 	train_labels_final[:len1,:]= train_labels
 	train_labels_final[len1: len1+ len2, :] = train_labels_acc
 	train_labels_final[len1 + len2:, :] = train_labels_acc2
-	print train_final.shape
-	print train_labels_final.shape
+
+
 	# Initiate kNN, train the data, then test it with test data for k=1
 	knn = cv2.ml.KNearest_create()
 	knn.train(train_final,cv2.ml.ROW_SAMPLE, train_labels_final)
-	# knn.train(train2, cv2.ml.ROW_SAMPLE, train_labels_acc)
-	# knn.train(train3, cv2.ml.ROW_SAMPLE, train_labels_acc2)
 	return knn
-
-
-# # classify all given notes using the given knn
-# # for testing purposes!!!! (includes pregiven testing labels)
-# def classifyNotesDebug(testingData, testingLabels, knn):
-
-# 	test = testingData[:,:].reshape(-1, 400).astype(np.float32)
-# 	ret,result,neighbours,dist = knn.findNearest(test,k=3)
-# 	print "result:\n", result
-# 	print "expected: \n", testingLabels
-
-# 	matches = result==testingLabels
-# 	correct = np.count_nonzero(matches)
-# 	accuracy = correct*100.0/result.size
-# 	print "accuracy:", accuracy
-
-# 	return result
-
-
-# # classify all given notes using the given knn
-# # not given testing labels to compare
-# def classifyNotes(testingData, knn):
-
-# 	test = testingData[:,:].reshape(-1, 400).astype(np.float32)
-# 	ret,result,neighbours,dist = knn.findNearest(test,k=3)
-# 	print "result:\n", result
-
-# 	return result
-
-
-# # converts image to grayscaled inverted image to be used in knn
-# # input: name of image file
-# def getTrainingData(imageFileName):
-# 	i1 = Image.open(imageFileName).convert('RGB') # each image 20*20= 400
-# 	cv_1 = np.array(i1)
-# 	cv_1 = cv_1[:, :, ::-1].copy()
-# 	cv_1 =  cv2.cvtColor(cv_1, cv2.COLOR_BGR2GRAY)
-# 	t1 = cv2.adaptiveThreshold(cv_1,255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 3)	
-# 	return t1
-
-
-# # add training image to the cells array and makes np array
-# # input: list of image filenames -- for testing purposes!!!
-# def makeTestingData(imageNameList):
-# 	cells = np.empty((0,20), int)
-# 	for imageName in imageNameList:
-# 		i1 = Image.open(imageName).convert('RGB') # each image 25 x 50 = 1250
-# 		cv_1 = np.array(i1)
-# 		cv_1 = cv_1[:, :, ::-1].copy()
-# 		cv_1 = cv2.resize(cv_1, (20, 20))
-# 		cv_1 =  cv2.cvtColor(cv_1, cv2.COLOR_BGR2GRAY)
-# 		t1 = cv2.adaptiveThreshold(cv_1,255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 5)
-# 		cells = np.append(cells, t1, axis=0)
-# 	return cells
-
-
-# # makes expected testing labels in column format
-# # for testing purposes!!
-# def makeTestingLabels(test_labels_list):
-# 	test_labels =  np.array(test_labels_list)
-# 	test_labels = np.tile(test_labels, 1)[:, np.newaxis]
-# 	return test_labels # to make column vector
 
 
 if __name__ == '__main__':
